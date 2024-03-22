7eb1a7b93c6055d3a2710fb0e02740a8465b936e
Michael Short <mshort0403@gmail.com>
2024-03-22 08:23:48 -0700

    Update bubble sort

--- bubble-sort.js
+++ bubble-sort.js
@@ -8,7 +8,6 @@ Space complexity: O(1) - constant
 function bubbleSort(arr) {
 	for (let i = 0; i < arr.length; i++) {
 		for (let j = 0; j < arr.length - i - 1; j++) {
-			console.log(i, j, arr, arr[j], arr[j + 1]);
 			if (arr[j] > arr[j + 1]) {
 				[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
 			}
